3d02b1e1a854a6f0f9950dd26bec8248ec8d07b7
Rahat Mazhar <rahatm0108@hstat.org>
2018-05-09 02:10:10 +0000

    fix jquery

--- script.js
+++ script.js
@@ -9,12 +9,12 @@
         console.log(health);
         if (health == "mental") {
           var dream = "Hello there " + name + " you decided to work on your mental health with the goal of" + power + ". Goodluck on your journey of self-improvement";
-          $('p2').html(dream).css('color', 'blue');
+          $('h4').html(dream).css('color', 'blue');
         
         }
         if (health== "physical") {
           var dream = "Hi there " + name + " you decided to work on your physical health with the goal of "+power;
-          $('p2').html(dream).css('color', 'green');
+          $('h4').html(dream).css('color', 'green');
           
         }
       });
